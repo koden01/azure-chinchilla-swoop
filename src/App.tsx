9c9a1824bfa219a521d99fcf3bd8c5ab644a7289
[dyad] <git@dyad.sh>
2025-06-13 08:42:06 +0700

    [dyad] Menambahkan future flags React Router. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardPage from "./pages/DashboardPage"; // Update import to DashboardPage
+import DashboardPage from "./pages/DashboardPage";
 import Input from "./pages/Input";
 import History from "./pages/History";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar"; // Import Navbar
+import Navbar from "./components/Navbar";
 
 const queryClient = new QueryClient();
 
@@ -16,12 +16,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <div className="flex flex-col min-h-screen">
-          <Navbar /> {/* Add Navbar here */}
+          <Navbar />
           <main className="flex-grow">
             <Routes>
-              <Route path="/" element={<DashboardPage />} /> {/* Use DashboardPage */}
+              <Route path="/" element={<DashboardPage />} />
               <Route path="/input" element={<Input />} />
               <Route path="/history" element={<History />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}

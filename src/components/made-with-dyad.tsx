142a4a2b219ba3ca440400911647675e4d5d5b13
koden <dedenkusdinar@gmail.com>
2025-06-14 14:35:14 +0700

    [dyad] Mengembalikan komponen `MadeWithDyad` yang terhapus. - wrote 1 file(s)

---
+++ src/components/made-with-dyad.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MadeWithDyad: React.FC = () => {
+  return (
+    <div className="w-full py-4 text-center text-sm text-gray-500 bg-gray-100 border-t border-gray-200">
+      Made with <span className="font-semibold text-blue-600">Dyad</span>
+    </div>
+  );
+};

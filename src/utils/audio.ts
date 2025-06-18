5505faec51b570591ab7e84aeb92cd1de6a49bd2
koden <dedenkusdinar@gmail.com>
2025-06-18 08:24:53 +0700

    [dyad] Removed the `beepCancel` audio object definition to fix a loading error. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,4 +1,3 @@
 export const beepSuccess = new Audio('/sounds/beep-success.mp3');
 export const beepFailure = new Audio('/sounds/beep-failure.mp3');
 export const beepDouble = new Audio('/sounds/beep-double.mp3');
-export const beepCancel = new Audio('/sounds/beep-cancel.mp3');

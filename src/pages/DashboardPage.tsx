4b1976d6fbe6c5fba4b9ee7ddb2a0a882cafa0f1
koden <dedenkusdinar@gmail.com>
2025-06-20 12:51:11 +0700

    [dyad] Menghapus komentar JSX yang menyebabkan kesalahan parsing di `DashboardPage.tsx`. - wrote 1 file(s)

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -49,7 +49,7 @@ const DashboardPage: React.FC = () => {
     handleCloseModal,
     handleBatalResi,
     handleConfirmResi,
-    handleCekfuToggle, // Corrected: Destructure as handleCekfuToggle
+    handleCekfuToggle,
   } = useDashboardModals({ date: selectedDate, formattedDate, allExpedisiData });
 
   const isLoadingAny =
@@ -120,7 +120,7 @@ const DashboardPage: React.FC = () => {
         selectedCourier={selectedCourier}
         onBatalResi={handleBatalResi}
         onConfirmResi={handleConfirmResi}
-        onCekfuToggle={handleCekfuToggle} {/* Corrected: Pass handleCekfuToggle */}
+        onCekfuToggle={handleCekfuToggle}
       />
     </React.Fragment>
   );
